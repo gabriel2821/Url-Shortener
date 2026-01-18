d30fc8196169dac023e73d34b66f0eaf17f6df43
gabriel2821 <gabrielvoo2821@gmail.com>
2026-01-18 23:49:42 +0800

    replace base_url to use request self url

--- backend/app/routers/url.py
+++ backend/app/routers/url.py
@@ -1,7 +1,7 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Request
 from sqlalchemy.orm import Session
 from fastapi.responses import RedirectResponse
-import os
+
 
 from app.core.database import get_db
 from app.models.url import URL
@@ -9,16 +9,16 @@ from app.schemas.url import URLCreate, URLResponse
 from app.utils.shortener import generate_short_code
 
 router = APIRouter()
-BASE_URL = os.getenv("BASE_URL")
+
 
 
 @router.post("/shorten", response_model=URLResponse)
-def shorten_url(data: URLCreate, db: Session = Depends(get_db)):
+def shorten_url(data: URLCreate, request: Request, db: Session = Depends(get_db)):
     #Prevent duplicate URLs
     original_url = str(data.original_url)
     existing = db.query(URL).filter(URL.original_url == original_url).first()
     if existing:
-        return {"short_url": f"{BASE_URL}/{existing.short_code}"}
+        return {"short_url": str(request.base_url) + existing.short_code}
 
     #Handle short code collision
     while True:
@@ -31,7 +31,7 @@ def shorten_url(data: URLCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(url)
 
-    return {"short_url": f"{BASE_URL}/{short_code}"}
+    return {"short_url": str(request.base_url) + short_code}
 
 @router.get("/{short_code}")
 def redirect_url(short_code: str, db: Session = Depends(get_db)):
